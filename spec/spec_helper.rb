0a3359d8da479faea2524c52b6a38e146fb618bd
Jeremy Goodrum <jeremy.b.goodrum@gmail.com>
2017-05-02 08:43:25 -0400

    FIX: issue with spec_helper and mocking supermarket accidentally.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,6 @@ require 'chefspec'
 require 'chefspec/berkshelf'
 require 'webmock/rspec'
 
-WebMock.disable_net_connect!(allow_localhost: true)
+WebMock.allow_net_connect!
 
 at_exit { ChefSpec::Coverage.report! }
